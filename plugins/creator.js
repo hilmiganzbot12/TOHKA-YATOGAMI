89f523c8e94e0c89c47e867f0c137640b502f8b0
IyanZ-XcX <101798961+IyanZ-XcX@users.noreply.github.com>
2022-03-29 00:37:02 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
     "displayName": "Iyann Ganteng",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Owner Tohka Yatogami\nitem1.TEL;waid=6289625556161:6289625556161\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:kastaraiyan@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/IyanZ-XcX\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia\nitem5.X-ABLabel:───────[ Tohka Yatogami ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Iyann Ganteng\nitem1.TEL;waid=6285742344873:6285742344873\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:kastaraiyan@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/IyanZ-XcX\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia\nitem5.X-ABLabel:───────[ Tohka Yatogami ]───────\nEND:VCARD"
   }, {
     "displayName": "Whatsapp",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
