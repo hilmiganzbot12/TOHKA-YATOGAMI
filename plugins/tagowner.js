e7e35bb1d4dc1493646e7e3591c48b327b0010c3
IyanZ-XcX <101798961+IyanZ-XcX@users.noreply.github.com>
2022-03-30 13:08:09 +0700

    Update tagowner.js

--- plugins/tagowner.js
+++ plugins/tagowner.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-conn.reply(global.owner[0] + `@s.whatsapp.net`, ` *@6289625556161 Ada yang memanggil anda :V*`, m)
+conn.reply(global.owner[0] + `@s.whatsapp.net`, ` *@6285742344873 Ada yang Manggil tuh*`, m)
 
   conn.reply(m.chat, `
-Kak Pilar⁩ (@6289625556161  ), Ada Yang Manggil anda😊
+Onii Chan! (@6285742344873  ), Dipanggil Temenmu!
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@6289625556161 /i
+handler.customPrefix = /@6285742344873 /i
 handler.command = new RegExp
 
 module.exports = handler
