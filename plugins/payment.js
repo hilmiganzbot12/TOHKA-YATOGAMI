2b55be48a95a4a589b414b6e1e20ad98a1d272d6
hilmiganzbot12 <96254454+hilmiganzbot12@users.noreply.github.com>
2022-03-31 15:24:20 +0700

    Update payment.js

--- plugins/payment.js
+++ plugins/payment.js
@@ -3,10 +3,46 @@ let handler = async (m, { conn, text }) => {
   let ext= `
 ╭═══ *〘 𝗣𝗮𝘆𝗺𝗲𝗻𝘁 〙*
 ║ ┅ ๑————————————๑
-║┊ ⌲ *OVO* : _085742344873_ 
-║┊ ⌲ *DANA* : _085742344873_
-║┊ ⌲ *TRI3* : _085742344873_
-╰═ ┅ ═══════
+
+PAYMENT BY Black BotZ Official
+
+
+
+🏧 BANK
+
+1. BRI 
+
+	ㅁ 3653 0103 1536 539	
+
+💰 E-MONEY
+
+1. GOPAY
+
+	ㅁ 0821 4334 9069
+
+2. OVO
+
+	ㅁ 0821 4334 9069
+
+3. DANA
+
+	ㅁ 0821 4334 9069
+
+	
+
+Bank BRI 
+
+   A/N TYAS HARYATI     
+
+📲 PULSA
+
+Tidak tersedia
+
+	
+
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+
+
 ▌│█║▌║▌║║▌║▌║█│▌
 `.trim()
 conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'Payment')).buffer(), ext, `Tohka Yatogami`, 'Nomor Owner', '.owner', 'Sewa Bot', '.sewa', m)
